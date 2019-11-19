d52259e1bab3108bf01673f420df349c5f6b7910
ComplYue <compl.yue@icloud.com>
2019-11-19 22:50:07 +0800

    refactor to be scaffold

--- shell.nix
+++ shell.nix
@@ -1,7 +1,15 @@
 # this defines the Nix env to run this interactive Haskell project
-with (import ./. { });
+with (import (
+# to use a version of Hadui from github
+  builtins.fetchTarball {
+    url = "https://github.com/complyue/hadui/archive/0.1.0.rc3.tar.gz";
+    sha256 = "1pa13r33smzni1r263sbz3q2iay4qyavmcxpaslafzc1rh548hbj";
+  }
+
+  # to use the version of Hadui checked out locally
+  #../hadui
+) { });
 haskellPackages.shellFor {
-  packages = p: with p; [ hadui-demo ];
-  nativeBuildInputs = [ pkgs.cabal-install ];
-  #withHoogle = true;
+  packages = p: [ p.zlib ];
+  nativeBuildInputs = [ pkgs.stack pkgs.hadui ];
 }
